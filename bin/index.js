a6d4fefb3056b9709c68a4f6accd5c728148b040
timkim <timk@adobe.com>
2025-02-11 15:46:39 -0800

    Put in express

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,23 @@
 #!/usr/bin/env node
 
 import { program } from "commander";
+import express from "express";
+
+const app = express();
+const port = 3000;
 
 program
   .version("1.0.0")
-  .description("My Node CLI")
-  .option("-n, --name <type>", "Add your name")
+  .description("adp-cli")
+  .option('-p, --port <number>', 'Port number', 3000)
   .action((options) => {
-    console.log(`Hey, ${options.name}!`);
+    app.get('/', (req, res) => {
+      res.send('Hello World!')
+    })
+    
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
+    })
   });
 
 program.parse(process.argv);

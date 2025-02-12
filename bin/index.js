b975480562501683448f80bfdd405aa08e3c6d53
timkim <timk@adobe.com>
2025-02-11 16:40:39 -0800

    Setting cli for middleware

--- bin/index.js
+++ bin/index.js
@@ -1,23 +1,14 @@
 #!/usr/bin/env node
 
 import { program } from "commander";
-import express from "express";
-
-const app = express();
-const port = 3000;
+import { middleware } from "../lib/middleware.js"
 
 program
   .version("1.0.0")
   .description("adp-cli")
   .option('-p, --port <number>', 'Port number', 3000)
   .action((options) => {
-    app.get('/', (req, res) => {
-      res.send('Hello World!')
-    })
-    
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`)
-    })
+    middleware(options);
   });
 
 program.parse(process.argv);
